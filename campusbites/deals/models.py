79efd9db38997d0e2851fea930048a2da87ac2db
johnnystouffer <stouff17@msu.edu>
2024-09-18 16:37:20 -0400

    added superuser and improving model

--- campusbites/deals/models.py
+++ campusbites/deals/models.py
@@ -1,3 +1,36 @@
+from decimal import Decimal
+from datetime import date, time, datetime
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    """
+    A specific post that gets added to our database.
+    """
+
+    # the price of the deal should be any from $0 to $99.99
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+
+    # a description of the food that you can get
+    description = models.CharField(max_length=40)
+
+    # what is the event?
+    event_name = models.CharField(max_length=25)
+
+    # where is the event (make sure that this is presice)
+    address = models.CharField(max_length=50)
+
+    # the amount of calories per dollar
+    calorie_per_dollar = models.DecimalField(max_digits=5, decimal_places=1)
+
+    # date and time of the event 
+    date_of_event = models.DateField()
+    time_of_event = models.TimeField()
+
+    # posted
+    submission_time = models.DateTimeField(auto_now_add=True)
+
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.event_name} at {self.address}"
